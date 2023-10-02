79ae769a0cc8ae5034fffbfe7eb68a434b52f72f
cuongkma <ngongocbiencuong@gmail.com>
2023-10-02 12:36:05 +0700

    .

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float m_speedPlayer;
+    private Rigidbody2D m_Rigidbody;
+    private float horizontal;
+    private float vertical;
+    private enum MovementState
+    {
+        PlayerIdle, VerticalMovement, HorizalMovement
+    }
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        // Tạo một Vector3 mới với giá trị horizontal và vertical
+        Vector3 movement = new Vector2(horizontal, vertical) * m_speedPlayer;
+
+        // Gán Vector3 làm vận tốc cho Rigidbody
+        m_Rigidbody.velocity = movement;
+    }
+}
