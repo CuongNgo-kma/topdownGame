8680a9cd7759bfcd65535af52f1527d64b6ef72f
cuongkma <ngongocbiencuong@gmail.com>
2023-10-02 13:33:59 +0700

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,24 @@
 ﻿using UnityEngine;
-
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float m_speedPlayer;
     private Rigidbody2D m_Rigidbody;
     private float horizontal;
     private float vertical;
+    private Animator anim;
+
     private enum MovementState
     {
-        PlayerIdle, VerticalMovement, HorizalMovement
+        idle, top, left, right, bot
     }
-
+    private MovementState State = MovementState.idle;
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,5 +31,27 @@ public class PlayerController : MonoBehaviour
 
         // Gán Vector3 làm vận tốc cho Rigidbody
         m_Rigidbody.velocity = movement;
+        StateMovement();
+    }
+    private void StateMovement()
+    {
+        if (horizontal >0f)
+        {
+            State = MovementState.right;
+        }
+        else if (horizontal <0f)
+        {
+            State = MovementState.left;
+        }
+        else if(vertical >0f)
+        {
+            State = MovementState.top;
+        }
+        else if (vertical <0f)
+        {
+            State = MovementState.bot;
+        }
+        else State = MovementState.idle;
+        anim.SetInteger("State", (int)State);
     }
 }
